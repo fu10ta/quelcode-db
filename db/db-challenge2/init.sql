f8c499fb46ad92bed9572c9f4ab446a4826ecb07
fu10ta <fu99ta@gmail.com>
2020-09-01 16:27:55 +0900

    first commit

---
+++ db/db-challenge2/init.sql
@@ -0,0 +1,73 @@
+CREATE TABLE `users` (
+      `user_id` int(11) NOT NULL AUTO_INCREMENT,
+      `user_name` varchar(100) NOT NULL,
+      `user_discription` varchar(1000),
+      `mobile_number` varchar(13),
+      `corporate_number` varchar(13),
+      `email` varchar(100) NOT NULL,
+      `password` varchar(100) NOT NULL,
+      `is_deleted` tinyint(1) NOT NULL DEFAULT 0,
+      `created_at` datetime NOT NULL,
+      `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+      PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE `members`(
+    `user_id` int(11) NOT NULL,
+    `room_id` int(11) NOT NULL,
+    `joined_at` datetime NOT NULL,
+    PRIMARY KEY (`user_id`,`room_id`),
+    FOREIGN KEY user_member_key(`user_id`) REFERENCES `users`(`user_id`),
+    FOREIGN KEY room_member_key(`room_id`) REFERENCES `rooms`(`room_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `rooms` (
+    `room_id` int(11) NOT NULL AUTO_INCREMENT,
+    `created_user_id` int(11) NOT NULL,
+    `updated_user_id` int(11) NOT NULL,
+    `room_name` varchar(100) NOT NULL,
+    `room_description` varchar(1000),
+    `is_sendable_file` tinyint(1) NOT NULL DEFAULT 1,
+    `is_direct_chat` tinyint(1) NOT NULL DEFAULT 0,
+    `is_deleted` tinyint(1) NOT NULL DEFAULT 0,
+    `created_at` datetime NOT NULL,
+    `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`room_id`),
+    FOREIGN KEY user_room_created_key(`created_user_id`) REFERENCES `users`(`user_id`),
+    FOREIGN KEY user_room_updated_key(`updated_user_id`) REFERENCES `users`(`user_id`),
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE `posts` (
+    `post_id` int(11) NOT NULL AUTO_INCREMENT,
+    `created_user_id` int(11) NOT NULL,
+    `updated_user_id` int(11) NOT NULL,
+    `room_id` int(11) NOT NULL,
+    `post_contents` varchar(1000),
+    `file_name` varchar(100),
+    `is_deleted` tinyint(1) NOT NULL DEFAULT 0,
+    `created_at` datetime NOT NULL,
+    `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`post_id`),
+    FOREIGN KEY user_post_created_key(`created_user_id`) REFERENCES `users`(`user_id`),
+    FOREIGN KEY user_post_updated_key(`updated_user_id`) REFERENCES `users`(`user_id`),
+    FOREIGN KEY room_post_key(`room_id`) REFERENCES `rooms`(`room_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE `tasks` (
+    `task_id` int(11) NOT NULL AUTO_INCREMENT,
+    `created_user_id` int(11) NOT NULL,
+    `updated_user_id` int(11) NOT NULL,
+    `admin_id` int(11) NOT NULL,
+    `room_id` int(11) NOT NULL,
+    `task_contents` varchar(1000) NOT NULL,
+    `limited_at` datetime,
+    `is_conpleted` tinyint(1) NOT NULL DEFAULT 0,
+    `is_deleted` tinyint(1) NOT NULL DEFAULT 0,
+    `created_at` datetime NOT NULL,
+    `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`task_id`),
+    FOREIGN KEY user_task_created_key(`created_user_id`) REFERENCES `users`(`user_id`),
+    FOREIGN KEY user_task_updated_key(`updated_user_id`) REFERENCES `users`(`user_id`),
+    FOREIGN KEY user_task_admin_key(`admin_id`) REFERENCES `users`(`user_id`),
+    FOREIGN KEY room_task_key(`room_id`) REFERENCES `rooms`(`room_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
