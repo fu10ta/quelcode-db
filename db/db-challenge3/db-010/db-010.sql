8c7b8599a26d2ee494b78e8c26538c3dbbc745d2
fu10ta <fu99ta@gmail.com>
2020-09-10 16:18:24 +0900

    レビュー修正

--- db/db-challenge3/db-010/db-010.sql
+++ db/db-challenge3/db-010/db-010.sql
@@ -1,16 +1,16 @@
 SELECT u.name AS ユーザー名, pp.投稿日時 AS 投稿日時, r.name AS チャットルーム名
-FROM posts p
-JOIN(
-    SELECT MAX(created_at) AS 投稿日時
+FROM (
+    SELECT MAX(created_at) AS 投稿日時,room_id
     FROM posts
     WHERE is_deleted = 0
     GROUP BY room_id
 )AS pp
-ON p.created_at = pp.投稿日時
+JOIN posts p
+ON pp.room_id = p.room_id
+AND 投稿日時 = p.created_at
 JOIN users u
 ON p.created_user_id = u.id
 JOIN rooms r
 ON p.room_id = r.id
 WHERE u.is_deleted =0
-AND p.is_deleted = 0
 ORDER BY r.id;
